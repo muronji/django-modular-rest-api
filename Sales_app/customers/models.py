f6757b30d1f247dc07e4b2633f457c00b69bb8da
muronji <muronjimulievi@gmail.com>
2026-01-27 11:49:42 +0300

    added customer seriaalizer and removed address from customer model

--- Sales_app/customers/models.py
+++ Sales_app/customers/models.py
@@ -5,7 +5,6 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=30)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
-    address = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
