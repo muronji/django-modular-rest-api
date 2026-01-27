d90df4da8dd25fa2b003220717de510b1a374dea
muronji <muronjimulievi@gmail.com>
2026-01-27 12:46:59 +0300

    created app urls and included them in the project urls

--- Sales_app/Sales_app/urls.py
+++ Sales_app/Sales_app/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from . import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('customers.urls')),
+    path('api/', include('orders.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
